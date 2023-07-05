73a7a50afb0979f84ac36c59cf4e0b03272d61fd
G-Romanczuk <gxbiromanczuk@gmail.com>
2023-07-05 05:19:24 +0200

    lunctionality create & list

--- projects-app/src/app/app.module.ts
+++ projects-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FunctionalityListComponent } from './functionality-list/functionality-list.component';
@@ -15,12 +15,25 @@ import { TaskDeleteComponent } from './task-delete/task-delete.component';
 import { TaskCreateComponent } from './task-create/task-create.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-
+//material
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+//service
 import { FunctionalityService } from './services/functionality.service';
 import { TaskService } from './services/task.service';
 
@@ -37,14 +50,33 @@ import { TaskService } from './services/task.service';
     TaskEditComponent,
     TaskDeleteComponent,
     TaskCreateComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatIconModule, MatListModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule, 
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSnackBarModule ,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatTableModule,
+    MatIconModule,
+    MatSortModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatDialogModule,
+    MatToolbarModule
+  
   ],
   providers: [FunctionalityService, TaskService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- projects-app/src/app/functionality-create/functionality-create.component.ts
+++ projects-app/src/app/functionality-create/functionality-create.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Kanban } from '../enums/kanban.enum';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Functionality } from '../interfaces/functionality';
+import { FunctionalityService } from '../services/functionality.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-functionality-create',
@@ -6,5 +14,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./functionality-create.component.css']
 })
 export class FunctionalityCreateComponent {
+  enumValues = Object.values(Kanban)
+  userValues = ["Gabi"]
+  projectValues = ["Default"]
+  functionalityForm! : FormGroup;
+  addedDateCustom : any 
+  
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private functionalityService : FunctionalityService,
+    private snackBar: MatSnackBar,
+    ) 
+    {
+      
+      
+    }
+
+    ngOnInit() {
+
+
+
+      const currentDate = new Date().toISOString().split('T')[0];
+      this.addedDateCustom = new FormControl({ value: currentDate, disabled: true });
+
+      console.log(currentDate)
+
+      this.functionalityForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        description: ['', Validators.required],
+        priority: ['', Validators.required],
+        projectName: [''], 
+        owner: ['', Validators.required],
+        kanban: [Kanban.Todo, Validators.required],
+        addedDate: this.addedDateCustom,
+        startDate: [''],
+        endDate: [''],
+        timeSpent: ['']
+      });
+      
+    }
+
+    createFunctionality(){
+     
+
+      const selectedProjectName = this.functionalityForm.value.projectName;
+
+      const selectedOwnerLogin = this.functionalityForm.value.owner;
+      
+  
+
+
+      const functionality: Functionality = {
+        ID: Date.now().toString(),
+        name: this.functionalityForm.value.name,
+        description: this.functionalityForm.value.description,
+        kanban: this.functionalityForm.value.kanban,
+        project: 'Default Project',
+        user: 'Gabi'
+      };
+
+     
+      this.functionalityService.createFunctionality(functionality).subscribe(
+        () => {
+          this.snackBar.open('Funkcjonalność została utworzona', 'OK', {
+            duration: 2000
+          });
+          this.functionalityForm.reset();
+        },
+        error => {
+          this.snackBar.open(
+            'Wystąpił błąd podczas tworzenia funkcjonalności',
+            'OK',
+            {
+              duration: 2000
+            }
+          );
+          console.log(error)
+        }
+      );
+
+      this.router.navigate(['/functionality/list']);
+
+    }
 }
