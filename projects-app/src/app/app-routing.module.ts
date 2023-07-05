1a86024d121a3959c7ab184739d7230242f5c843
G-Romanczuk <gxbiromanczuk@gmail.com>
2023-07-05 02:19:25 +0200

    service & routes

--- projects-app/src/app/app-routing.module.ts
+++ projects-app/src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+import { FunctionalityCreateComponent } from './functionality-create/functionality-create.component';
+import { TaskCreateComponent } from './task-create/task-create.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { FunctionalityListComponent } from './functionality-list/functionality-list.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+import { FunctionalityEditComponent } from './functionality-edit/functionality-edit.component';
+
+
+
+const routes: Routes = [
+  {
+    path: "functionality/list", component: FunctionalityListComponent,
+   },
+  {
+    path:"functionality/create",component:FunctionalityCreateComponent,
+  },
+  {
+    path: "functionality/edit", component: FunctionalityEditComponent,
+   },
+   {
+    path: "task/list", component: TaskListComponent,
+   },
+  {
+    path:"task/create",component:TaskCreateComponent,
+  },
+  {
+    path: "task/edit", component: TaskEditComponent,
+   },
+
+
+  {path:"",redirectTo:"functionality/list", pathMatch:"full"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
